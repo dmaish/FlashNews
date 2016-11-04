d24b028b616cda8d2b609d7b668e71f6f030ed87
dmaish <mainadaniel81@gmail.com>
2016-11-04 10:09:34 +0300

    "recyclerViews working but only one recyclerView Item getting displayed"

--- app/src/main/java/com/example/danielmaina/flashnews/MyAdapter.java
+++ app/src/main/java/com/example/danielmaina/flashnews/MyAdapter.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -36,6 +38,13 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
+        //storing current Item
+        FeedItem current=feedItems.get(position);
+        //accessing the news Items  on the custom layout using the ViewHolder object
+        holder.Title.setText(current.getTitle());
+        holder.Description.setText(current.getDescription());
+        holder.Date.setText(current.getPubDate());
+
 
     }
 
@@ -46,8 +55,18 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
+        //initialising the textViews and ImageViews in the cardView for the recyclerView's use
+        TextView Title,Description,Date;
+        ImageView Thumbnail;
+
+
         public MyViewHolder(View itemView) {
             super(itemView);
+            //initialising their Xml layouts
+            Title=(TextView) itemView.findViewById(R.id.title_text);
+            Description=(TextView) itemView.findViewById(R.id.description_text);
+            Thumbnail=(ImageView) itemView.findViewById(R.id.thumb_img);
+            Date=(TextView) itemView.findViewById(R.id.date_text);
         }
     }
 }
