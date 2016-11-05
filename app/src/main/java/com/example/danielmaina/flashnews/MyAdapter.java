2cb1f61cbd0b2f94835515cf75be462ee52e21f5
dmaish <mainadaniel81@gmail.com>
2016-11-05 14:59:43 +0300

    "cardView rubberband animation from third party library added"

--- app/src/main/java/com/example/danielmaina/flashnews/MyAdapter.java
+++ app/src/main/java/com/example/danielmaina/flashnews/MyAdapter.java
@@ -1,12 +1,16 @@
 package com.example.danielmaina.flashnews;
 
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 import java.util.ArrayList;
 
@@ -38,6 +42,10 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
+        //linking cardView with xml to initiate animationss(third parties)
+        YoYo.with(Techniques.RubberBand)
+                .playOn(holder.cardView);
+
         //storing current Item
         FeedItem current=feedItems.get(position);
         //accessing the news Items  on the custom layout using the ViewHolder object
@@ -60,9 +68,11 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
         ImageView Thumbnail;
 
 
+        CardView cardView;
         public MyViewHolder(View itemView) {
             super(itemView);
             //initialising their Xml layouts
+            cardView=(CardView) itemView.findViewById(R.id.cardview);
             Title=(TextView) itemView.findViewById(R.id.title_text);
             Description=(TextView) itemView.findViewById(R.id.description_text);
             Thumbnail=(ImageView) itemView.findViewById(R.id.thumb_img);
